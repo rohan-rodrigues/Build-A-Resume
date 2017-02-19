0f9f272e9e74672010c599569946a504d76984de
sbgs9 <ganeshsusarla01@gmail.com>
2017-02-19 11:55:46 -0800

    Created login and register activities with functionality

---
+++ app/src/main/java/com/example/rohanrodrigues/build_a_resume/MainUserActivity.java
@@ -0,0 +1,13 @@
+package com.example.rohanrodrigues.build_a_resume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_user);
+    }
+}
